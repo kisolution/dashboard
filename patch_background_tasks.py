b0a4d63e4db225ae2e855ed9ce520a25a87521a5
usmanjohn <backpacker0220@gmail.com>
2024-08-25 19:03:02 +0900

    Update patch for django-background-tasks

--- patch_background_tasks.py
+++ patch_background_tasks.py
@@ -4,19 +4,23 @@ import sys
 def patch_compat():
     compat_path = os.path.join(sys.prefix, 'lib', 'python3.12', 'site-packages', 'compat', '__init__.py')
     with open(compat_path, 'r') as file:
-        content = file.read()
+        content = file.readlines()
     
-    content = content.replace(
-        "from django.conf.urls import url, include, handler404, handler500",
-        "from django.urls import re_path as url, include\nfrom django.conf.urls import handler404, handler500"
-    )
-    content = content.replace(
-        "from django.conf.urls.defaults import url, include, handler404, handler500",
-        "from django.urls import re_path as url, include\nfrom django.conf.urls import handler404, handler500"
-    )
+    new_content = []
+    for line in content:
+        if line.strip().startswith('from django.conf.urls import'):
+            new_content.append("from django.urls import re_path as url, include\n")
+            new_content.append("from django.urls import path\n")
+            new_content.append("from django.conf.urls import handler404, handler500\n")
+        elif line.strip().startswith('from django.conf.urls.defaults import'):
+            new_content.append("from django.urls import re_path as url, include\n")
+            new_content.append("from django.urls import path\n")
+            new_content.append("from django.conf.urls import handler404, handler500\n")
+        else:
+            new_content.append(line)
     
     with open(compat_path, 'w') as file:
-        file.write(content)
+        file.writelines(new_content)
 
 if __name__ == "__main__":
     patch_compat()
